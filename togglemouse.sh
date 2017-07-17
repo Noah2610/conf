7cb07f93040489ebaab60990f66ac39e9fb52d99
Noah <noah@rays-lab.com>
2017-07-17 23:39:39 +0200

    i3: added mouse enabled state toggle bash script and assigned keybind to it

---
+++ togglemouse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+id=10
+state=$( xinput --list-props $id | grep "Device Enabled" | cut -d ":" -f 2 | tr -d "\t" )
+timeout=5
+width=$( xdpyinfo | awk '/dimensions/{print $2}' | cut -d"x" -f1 )
+height=$( xdpyinfo | awk '/dimensions/{print $2}' | cut -d"x" -f2 )
+case "$state" in
+	"0")
+		xinput --enable $id
+		pkill unclutter
+		unclutter -root -idle $timeout &
+		xdotool mousemove $( echo "$width / 2" | bc ) $( echo "$height / 2" | bc )
+	;;
+	"1")
+		xinput --disable $id
+		pkill unclutter
+		unclutter -root -idle 0.01 &
+		xdotool mousemove $width 0
+	;;
+esac
+
