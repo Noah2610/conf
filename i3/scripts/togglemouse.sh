b54199879ddd527e99b30ce9f007cfefe5d43d52
Noah Rosenzweig <noah@rays-lab.com>
2017-09-18 17:26:26 +0200

    togglemouse: en/disable touchpad with script aswell now; i3conf: moved acer script to scripts/ directory

--- i3/scripts/togglemouse.sh
+++ i3/scripts/togglemouse.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
-#PROFILE=h77m
-id=10
-#PROFILE=acer
-##id=$( xinput --list "pointer:Logitech USB Receiver" | cut -d$'\n' -f1 | cut -d$'\t' -f2 | cut -d"=" -f2 )
-state=$( xinput --list-props $id | grep "Device Enabled" | cut -d ":" -f 2 | tr -d "\t" )
+statefilePath="/home/noah/.config/i3/mousestate"
+#PROFILE_START=h77m
+##id=10
+##state=$( xinput --list-props $id | grep "Device Enabled" | cut -d ":" -f 2 | tr -d "\t" )
+#PROFILE_END
+#PROFILE_START=acer
+if xinput --list "pointer:Logitech USB Receiver" ; then
+	id=$( xinput --list "pointer:Logitech USB Receiver" | cut -d$'\n' -f1 | cut -d$'\t' -f2 | cut -d"=" -f2 )
+	state=$( xinput --list-props $id | grep "Device Enabled" | cut -d ":" -f 2 | tr -d "\t" )
+else
+	state=$( cat $statefilePath )
+fi
+#PROFILE_END
 timeout=5
 width=$( xdpyinfo | awk '/dimensions/{print $2}' | cut -d"x" -f1 )
 height=$( xdpyinfo | awk '/dimensions/{print $2}' | cut -d"x" -f2 )
 #PROFILE_START=acer
-##width=$( echo "$width + $width / 2" | bc )
-##height=$( echo "$height + $height / 2" | bc )
+width=$( echo "$width + $width / 2" | bc )     # < gotta fix this
+height=$( echo "$height + $height / 2" | bc )  # <
 #PROFILE_END
 case "$state" in
 	"0")
+		echo "STATE 0"
 		xinput --enable $id
 		pkill unclutter
 		#unclutter -root -noevents -idle $timeout &
 		xdotool mousemove $( echo "$width / 2" | bc ) $( echo "$height / 2" | bc )
-		echo -n "1" > /home/noah/.config/i3/mousestate
+		#PROFILE=acer
+		synclient TouchpadOff=0
+		echo -n "1" > $statefilePath
 	;;
 	"1")
+		echo "STATE 1"
 		xinput --disable $id
 		pkill unclutter
 		unclutter -root -noevents -idle 0 &
 		xdotool mousemove $width 0
-		echo -n "0" > /home/noah/.config/i3/mousestate
+		synclient TouchpadOff=1
+		echo -n "0" > $statefilePath
 	;;
 esac
 
+echo "TETS"
