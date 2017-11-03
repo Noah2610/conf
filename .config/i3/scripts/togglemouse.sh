dd5f63082e27d517bbb8fd40faaaf15b001d5e24
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2017-11-03 11:47:13 +0100

    i3: improved togglemouse.sh, only call xrandr once

--- .config/i3/scripts/togglemouse.sh
+++ .config/i3/scripts/togglemouse.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 statefilePath="/home/noah/.config/i3/mousestate"
-#PROFILE_START=h77m,h77m-arch
-##main_display="DVI-D-0"
-##display="DVI-I-0"
+#timeout=5
+#PROFILE_START=h77m-arch
 ##id=10
 ##state=$( xinput --list-props $id | grep "Device Enabled" | cut -d ":" -f 2 | tr -d "\t" )
+##main_display="DVI-D-0"
+##display="DVI-I-0"
+##output=$( xrandr | grep -E "^$main_display|^$display"  )
+##main_display_output=$( echo "$output" | grep -E "^$main_display" | cut -d" " -f4 | cut -d"+" -f1 )
+##display_output=$( echo "$output" | grep -E "^$display" | cut -d" " -f3 | cut -d"+" -f1 )
+##main_display_width=$( echo "$main_display_output" | cut -d"x" -f1 )
+##main_display_height=$( echo "$main_display_output" | cut -d"x" -f2 )
+##display_width=$( echo "$display_output" | cut -d"x" -f1 )
+##display_height=$( echo "$display_output" | cut -d"x" -f2 )
 #PROFILE_END
 #PROFILE_START=acer
-main_display="DP-1"
-display="eDP-1"
 if xinput --list "pointer:Logitech USB Receiver" ; then
 	id=$( xinput --list "pointer:Logitech USB Receiver" | cut -d$'\n' -f1 | cut -d$'\t' -f2 | cut -d"=" -f2 )
 	state=$( xinput --list-props $id | grep "Device Enabled" | cut -d ":" -f 2 | tr -d "\t" )
 else
 	state=$( cat $statefilePath )
 fi
-#PROFILE_END
-#timeout=5
-#PROFILE_START=h77m-arch
-##main_display_width=$( xrandr | grep -E "^$main_display" | cut -d" " -f4 | cut -d"+" -f1 | cut -d"x" -f1 )
-##main_display_height=$( xrandr | grep -E "^$main_display" | cut -d" " -f4 | cut -d"+" -f1 | cut -d"x" -f2 )
-##display_width=$( xrandr | grep -E "^$display" | cut -d" " -f3 | cut -d"+" -f1 | cut -d"x" -f1 )
-##display_height=$( xrandr | grep -E "^$display" | cut -d" " -f3 | cut -d"+" -f1 | cut -d"x" -f2 )
-#PROFILE_END
-#PROFILE_START=acer
-main_display_width=$( xrandr | grep -E "^$main_display" | cut -d" " -f3 | cut -d"+" -f1 | cut -d"x" -f1 )
-main_display_height=$( xrandr | grep -E "^$main_display" | cut -d" " -f3 | cut -d"+" -f1 | cut -d"x" -f2 )
-display_width=$( xrandr | grep -E "^$display" | cut -d" " -f4 | cut -d"+" -f1 | cut -d"x" -f1 )
-display_height=$( xrandr | grep -E "^$display" | cut -d" " -f4 | cut -d"+" -f1 | cut -d"x" -f2 )
+main_display="DP-1"
+display="eDP-1"
+output=$( xrandr | grep -E "^$main_display|^$display"  )
+main_display_output=$( echo "$output" | grep -E "^$main_display" | cut -d" " -f3 | cut -d"+" -f1 )
+display_output=$( echo "$output" | grep -E "^$display" | cut -d" " -f4 | cut -d"+" -f1 )
+main_display_width=$( echo "$main_display_output" | cut -d"x" -f1 )
+main_display_height=$( echo "$main_display_output" | cut -d"x" -f2 )
+display_width=$( echo "$display_output" | cut -d"x" -f1 )
+display_height=$( echo "$display_output" | cut -d"x" -f2 )
 #PROFILE_END
 mouse_x=$( echo "$display_width + ($main_display_width / 2)" | bc )
 mouse_y=$( echo "$main_display_height / 2" | bc )
