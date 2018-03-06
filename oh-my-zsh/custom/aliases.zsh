b89f3e85db29dd59394fc427f031eb32ff18495a
Noah <rosenzweig.noah@gmail.com>
2018-03-06 02:20:43 +0100

    Added zsh and oh-my-zsh configs; i3conf: Set shell to zsh for h77m-arch

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,23 @@
+
+## Configs
+alias ezsh="$EDITOR $HOME/.zshrc"
+alias rzsh="ranger $ZSH/custom"
+alias evim="$EDITOR $HOME/.vimrc"
+alias szsh="source $HOME/.zshrc"
+
+## Git
+alias gis="git status"
+alias gip="git push"
+alias gipu="git pull"
+alias gipup="git push -u"
+alias gif="git fetch --all"
+alias gica="git add -A; git commit -m"
+alias gicam="git add -A; git commit --amend"
+alias gil="git log"
+alias gi1="git log --oneline"
+alias gitree="git log --oneline --decorate --graph --all"
+
+## misc
+alias psa="ps aux | grep -i"
+
+
---
+++ oh-my-zsh/custom/vimode.zsh
@@ -0,0 +1,6 @@
+
+## Set vi mode
+bindkey -v
+# Set escape key timeout to 0.25 seconds
+export KEYTIMEOUT=25
+
