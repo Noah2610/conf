b4bd06f1740f6c9f76a86d7fb76ea0f54d666f26
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2018-07-05 16:02:06 +0200

    i3blocks/get-cmus.rb: Update time bar unicode characters

--- misc/slack-dark-theme/custom.js
+++ misc/slack-dark-theme/custom.js
@@ -9,46 +9,46 @@
 // CUSTOM DARK THEME //
 // First make sure the wrapper app is loaded
 document.addEventListener("DOMContentLoaded", function() {
-	// Then get its webviews
-	let webviews = document.querySelectorAll(".TeamView webview");
+  // Then get its webviews
+  let webviews = document.querySelectorAll(".TeamView webview");
 
-	// Fetch our CSS in parallel ahead of time
-	const cssPath = 'https://cdn.rawgit.com/widget-/slack-black-theme/master/custom.css';
-	let cssPromise = fetch(cssPath).then(response => response.text());
-	let customCustomCSS = `
-		:root {
-			--primary:             #61AFEF;
-			--text:                #ABB2BF;
-			--background:          #282C34;
-			--background-elevated: #3B4048;
-		}
-	`
+  // Fetch our CSS in parallel ahead of time
+  const cssPath = 'https://cdn.rawgit.com/widget-/slack-black-theme/master/custom.css';
+  let cssPromise = fetch(cssPath).then(response => response.text());
+  let customCustomCSS = `
+    :root {
+      --primary:             #61AFEF;
+      --text:                #ABB2BF;
+      --background:          #282C34;
+      --background-elevated: #3B4048;
+    }
+  `
 
-	// Insert a style tag into the wrapper view
-	cssPromise.then(css => {
-		let s = document.createElement('style');
-		s.type = 'text/css';
-		s.innerHTML = css + customCustomCSS;
-		document.head.appendChild(s);
-	});
+  // Insert a style tag into the wrapper view
+  cssPromise.then(css => {
+    let s = document.createElement('style');
+    s.type = 'text/css';
+    s.innerHTML = css + customCustomCSS;
+    document.head.appendChild(s);
+  });
 
-	// Wait for each webview to load
-	webviews.forEach(webview => {
-		webview.addEventListener('ipc-message', message => {
-			if (message.channel == 'didFinishLoading')
-				// Finally add the CSS into the webview
-				cssPromise.then(css => {
-					let script = `
-					let s = document.createElement('style');
-					s.type = 'text/css';
-					s.id = 'slack-custom-css';
-					s.innerHTML = \`${css + customCustomCSS}\`;
-					document.head.appendChild(s);
-					`
-					webview.executeJavaScript(script);
-				})
-		});
-	});
+  // Wait for each webview to load
+  webviews.forEach(webview => {
+    webview.addEventListener('ipc-message', message => {
+      if (message.channel == 'didFinishLoading')
+        // Finally add the CSS into the webview
+        cssPromise.then(css => {
+          let script = `
+          let s = document.createElement('style');
+          s.type = 'text/css';
+          s.id = 'slack-custom-css';
+          s.innerHTML = \`${css + customCustomCSS}\`;
+          document.head.appendChild(s);
+          `
+          webview.executeJavaScript(script);
+        })
+    });
+  });
 });
 
 
@@ -60,10 +60,10 @@ document.addEventListener("DOMContentLoaded", function() {
 ////////////////////////////////////////////////////////////////////////////////
 
 document.addEventListener('DOMContentLoaded', function() {
-	$.ajax({
-		url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
-		success: function(css) {
-			$("<style></style>").appendTo('head').html(css);
-		}
-	});
+  $.ajax({
+    url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
+    success: function(css) {
+      $("<style></style>").appendTo('head').html(css);
+    }
+  });
 });
