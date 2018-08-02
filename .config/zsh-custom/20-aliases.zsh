28a9a74f3c591399e666771025e62b71e21527ee
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2018-08-02 15:08:10 +0200

    zsh: Update some config files

--- .config/zsh-custom/20-aliases.zsh
+++ .config/zsh-custom/20-aliases.zsh
@@ -1,11 +1,11 @@
-## Configs
+# Configs
 alias ezsh="$EDITOR $HOME/.zshrc"
 alias evim="$EDITOR $HOME/.vimrc"
 alias rzsh="ranger $HOME/.config/zsh-custom"
 alias ri3="ranger $HOME/.config/i3"
 alias szsh="source $HOME/.zshrc"
 
-## Git
+# Git
 alias gis="git status"
 alias gip="git push"
 alias gipf="git push --force"
@@ -20,8 +20,16 @@ alias gi1="git log --oneline"
 alias gitree="git log --oneline --decorate --graph --all"
 alias gich="git checkout"
 
-## misc
+# Ranger
+alias ra="ranger"
+alias rah="ranger $HOME"
+alias rad="ranger $HOME/Downloads"
+alias rac="ranger $HOME/.config"
+alias raC="ranger $HOME/conf"
+alias rai3="ranger $HOME/.config/i3"
+alias ram="ranger $HOME/misc"
+
+# misc
 alias psa="ps aux | grep -i"
-alias rg="ranger"
 alias cdpath='cd "$( xclip -o -selection clipboard )"'
 alias vall='if [ -f "bin/vimall" ]; then bin/vimall; elif [ -f "bin/vimall.sh" ]; then bin/vimall.sh; else false; fi'
