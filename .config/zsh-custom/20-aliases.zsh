062ab72de04bb1967ef7cae21ee7ad544e37db7b
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2018-09-10 19:32:05 +0200

    zsh: Added cd aliases

--- .config/zsh-custom/20-aliases.zsh
+++ .config/zsh-custom/20-aliases.zsh
@@ -20,6 +20,19 @@ alias gi1="git log --oneline"
 alias gitree="git log --oneline --decorate --graph --all"
 alias gich="git checkout"
 
+# cd
+alias cdd="cd $HOME/Downloads"
+alias cdM="cd $HOME/misc"
+alias cdp="cd $HOME/Pictures"
+alias cds="cd $HOME/Pictures/Screenshots"
+alias cdg="cd $HOME/Games"
+alias cdc="cd $HOME/.config"
+alias cdC="cd $HOME/conf"
+alias cdi3="cd $HOME/.config/i3"
+alias cdz="cd $HOME/.config/zsh-custom"
+alias cdS="cd $HOME/Sync"
+alias cdS="cd $HOME/Sync/General"
+
 # Ranger
 alias ra="ranger"
 alias rah="ranger $HOME"
@@ -31,6 +44,9 @@ alias rag="ranger $HOME/Games"
 alias rac="ranger $HOME/.config"
 alias raC="ranger $HOME/conf"
 alias rai3="ranger $HOME/.config/i3"
+alias raz="ranger $HOME/.config/zsh-custom"
+alias raS="ranger $HOME/Sync"
+alias raG="ranger $HOME/Sync/General"
 
 # misc
 alias psa="ps aux | grep -i"
