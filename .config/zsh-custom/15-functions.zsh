3be2246a657d650746aaeae3277a4b62d993365a
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2018-12-14 01:40:26 +0100

    zsh-custom: use '\cd' in cd_then_source function

--- .config/zsh-custom/15-functions.zsh
+++ .config/zsh-custom/15-functions.zsh
@@ -3,12 +3,9 @@ function source_zshrc {
   [ -f "./zshrc" ] && source ./zshrc
 }
 
-# Unalias cd to avoid recursively calling 'cd_then_source' when cd alias was set before defining the function.
-alias cd && unalias cd
-
 # Change directory as usual with cd but execute function 'source_zshrc' afterwards
 function cd_then_source {
-  cd $@
+  \cd $@
   exit_code=$?
   source_zshrc
   return $exit_code
