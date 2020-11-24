59ebe668dfefb7a3a0b8890e8caecab36e62c847
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-11-24 06:57:54 +0100

    zsh/aliases: Remove a bunch of unused aliases

--- zsh/.config/zsh-custom/20-aliases.zsh
+++ zsh/.config/zsh-custom/20-aliases.zsh
@@ -53,32 +53,16 @@ alias gich="git checkout"
 
 # cd
 alias cd="cd_then_source"
-alias cdd="cd $HOME/Downloads"
-alias cdM="cd $HOME/misc"
-alias cdp="cd $HOME/Pictures"
-alias cds="cd $HOME/Pictures/Screenshots"
-alias cdg="cd $HOME/Games"
-alias cdc="cd $HOME/.config"
-alias cdC="cd $HOME/conf"
-alias cdi3="cd $HOME/.config/i3"
-alias cdz="cd $HOME/.config/zsh-custom"
-alias cdS="cd $HOME/Sync"
-alias cdS="cd $HOME/Sync/General"
 
 # ranger
 alias ra="ranger"
 alias rah="ranger $HOME"
 alias rad="ranger $HOME/Downloads"
-alias ram="ranger $HOME/misc"
-alias rap="ranger $HOME/Pictures"
-alias ras="ranger $HOME/Pictures/Screenshots"
+alias rap="ranger $HOME/Projects"
 alias rag="ranger $HOME/Games"
 alias rac="ranger $HOME/.config"
 alias raC="ranger $HOME/conf"
-alias rai3="ranger $HOME/.config/i3"
 alias raz="ranger $HOME/.config/zsh-custom"
-alias raS="ranger $HOME/Sync"
-alias raG="ranger $HOME/Sync/General"
 
 # misc
 alias psa="ps aux | grep -i"
